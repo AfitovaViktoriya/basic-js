ad2785a808359d13e4d853bdd0699cabda88e9df
AfitovaViktoriya <afitovaviktoria@gmail.com>
2022-04-26 16:30:49 +0300

    feat: add count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,23 +1,9 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Given matrix where you have to find cats by ears "^^"
- *
- * @param {Array<Array>} matrix
- * @return {Number} count of cats found
- *
- * @example
- * countCats([
- *  [0, 1, '^^'],
- *  [0, '^^', 2],
- *  ['^^', 1, 2]
- * ]) => 3`
- *
- */
 function countCats(matrix) {
   let template = matrix
-    .join(",")
-    .split(",")
+    .join(',')
+    .split(',')
     .filter(i => i === '^^');
   if (template.length !== 0) return template.length;
   else return 0;
@@ -28,6 +14,7 @@ let matrix = [
   ['^^', 1, 2]
 ]
 countCats(matrix);
+
 module.exports = {
   countCats
 };
