c775d35d5234b5b37b5ff63dae6d1b677bbc7181
AfitovaViktoriya <afitovaviktoria@gmail.com>
2022-04-27 10:34:27 +0300

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,34 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let names = [];
+  if (!Array.isArray(members)) return false;
+
+  members.forEach((el) => {
+    if (typeof el !== "string") return false;
+    let elemTrimmed = el.trim();
+    let elemToUpperCase = elemTrimmed[0].toUpperCase();
+    names.push(elemToUpperCase);
+  });
+
+  names.sort();
+  let result = names.join("");
+  return result;
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
